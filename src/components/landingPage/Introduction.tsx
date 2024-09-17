f477d972f18d55ed6d23b0d174f06be86da53953
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-09-17 19:12:49 +0330

    update

--- src/components/landingPage/Introduction.tsx
+++ src/components/landingPage/Introduction.tsx
@@ -6,12 +6,12 @@ const Introduction = () => {
     <div className="w-full h-[40rem] bg-black flex justify-center items-center">
       <div className="flex justify-center items-center relative">
         <img className="max-sm:hidden sm2:block" src={img} alt="intro-bg" />
-        <div className="absolute shadow-[0_0_8rem_7rem_#1B112B]"></div>
+        <div className="absolute shadow-[0_0_10rem_9rem_#1B112B]"></div>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.5, duration: 0.75 }}
-          className="absolute  flex flex-col items-center justify-center gap-4 max-sm:w-[20rem] max-sm2:w-[24rem] sm2:w-[35rem] md2:w-auto"
+          className="absolute flex flex-col items-center justify-center gap-4 max-sm:w-[20rem] max-sm2:w-[24rem] sm2:w-[35rem] md2:w-auto"
         >
           <h1 className="max-sm:text-[2rem] sm2:text-4xl font-bold text-white text-center">
             Cryptocurrency Price and Trend Monitor
