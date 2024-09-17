79a6cfb98aa423daf37409da7d0462dc4d5028d6
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-09-17 13:44:49 +0330

    update

--- src/components/landingPage/Introduction.tsx
+++ src/components/landingPage/Introduction.tsx
@@ -1,4 +1,5 @@
 import img from "../../assets/landingPage/Intro.webp";
+import { motion } from "framer-motion";
 
 const Introduction = () => {
   return (
@@ -6,7 +7,12 @@ const Introduction = () => {
       <div className="flex justify-center items-center relative">
         <img className="max-sm:hidden sm2:block" src={img} alt="intro-bg" />
         <div className="absolute shadow-[0_0_8rem_7rem_#1B112B]"></div>
-        <div className="absolute flex flex-col items-center justify-center gap-4 max-sm:w-[20rem] max-sm2:w-[24rem] sm2:w-[35rem] md2:w-auto">
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5, duration: 0.75 }}
+          className="absolute  flex flex-col items-center justify-center gap-4 max-sm:w-[20rem] max-sm2:w-[24rem] sm2:w-[35rem] md2:w-auto"
+        >
           <h1 className="max-sm:text-[2rem] sm2:text-4xl font-bold text-white text-center">
             Cryptocurrency Price and Trend Monitor
           </h1>
@@ -15,7 +21,7 @@ const Introduction = () => {
             tracker, designed to help you manage your portfolio and make
             informed investment decisions.
           </p>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
