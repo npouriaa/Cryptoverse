0414e6ac2ac5035db11b34a6dae5ae70dc5f0880
npouriaa <npouriaa@gmail.com>
2024-10-05 17:31:25 +0330

    add CoinDetailsType

--- src/types/DataTypes.ts
+++ src/types/DataTypes.ts
@@ -9,3 +9,25 @@ export type CoinType = {
   total_volume: number;
   market_cap_rank: number;
 };
+
+export type CoinDetailsType = {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  description: {
+    en: string;
+  };
+  market_data: {
+    price_change_percentage_24h: number;
+    total_volume: {
+      usd: number;
+    };
+    current_price: {
+      usd: number;
+    };
+    market_cap: {
+      usd: number;
+    };
+  };
+};
