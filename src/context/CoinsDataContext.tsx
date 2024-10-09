3429f77ede694bc70b8f66dbae7f2017bcc02573
npouriaa <npouriaa@gmail.com>
2024-10-09 15:41:08 +0330

    update : get coins list again whenever the Coins.tsx renders

--- src/context/CoinsDataContext.tsx
+++ src/context/CoinsDataContext.tsx
@@ -20,6 +20,7 @@ type CoinsDataContextType = {
   error: string;
   paginatedCoins: CoinType[];
   setPaginatedCoins: Dispatch<SetStateAction<CoinType[]>>;
+  getCoinsData?: () => Promise<void>;
 };
 
 const CoinsDataContext = createContext({} as CoinsDataContextType);
@@ -34,31 +35,31 @@ const CoinsDataContextProvider = ({
     [] as CoinType[]
   );
 
-  useEffect(() => {
-    const getCoinsData = async () => {
-      setLoading(true);
-      setError("");
-      try {
-        const res = await axios.get(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
-          options
-        );
-        setCoinsList(res.data);
-        setPaginatedCoins(res.data.slice(0, 10));
-        console.log(res.data);
-      } catch (err) {
-        console.log(err);
-        setError((err as Error).message);
-        setLoading(false);
-      }
+  const getCoinsData = async () => {
+    setLoading(true);
+    setError("");
+    try {
+      const res = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+        options
+      );
+      setCoinsList(res.data);
+      setPaginatedCoins(res.data.slice(0, 10));
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+      setError((err as Error).message);
       setLoading(false);
-    };
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
     getCoinsData();
   }, []);
 
   return (
     <CoinsDataContext.Provider
-      value={{ coinsList, loading, error, paginatedCoins, setPaginatedCoins }}
+      value={{ coinsList, loading, error, paginatedCoins, setPaginatedCoins , getCoinsData }}
     >
       {children}
     </CoinsDataContext.Provider>
--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,12 +1,18 @@
-import { ChangeEvent, useContext, useState } from "react";
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 import CoinsTable from "../components/CoinsTable";
 import { CoinsDataContext } from "../context/CoinsDataContext";
 import PaginationComponent from "../components/coins/PaginationComponent";
 import Search from "../components/coins/Search";
 
 const Coins = () => {
-  const { coinsList, loading, error, paginatedCoins, setPaginatedCoins } =
-    useContext(CoinsDataContext);
+  const {
+    coinsList,
+    loading,
+    error,
+    paginatedCoins,
+    setPaginatedCoins,
+    getCoinsData,
+  } = useContext(CoinsDataContext);
   const [search, setSearch] = useState<string>("");
   const [page, setPage] = useState<number>(1);
 
@@ -21,15 +27,19 @@ const Coins = () => {
     var initialCount = (value - 1) * 10;
     setPaginatedCoins(coinsList.slice(initialCount, initialCount + 10));
     window.scrollTo({
-      top : 0,
-      behavior : "smooth"
-    })
+      top: 0,
+      behavior: "smooth",
+    });
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e.target.value);
   };
+
+  useEffect(() => {
+    if (getCoinsData) getCoinsData();
+  }, []);
 
   return (
     <div className="min-h-[86.2vh] flex justify-start flex-col items-center max-sm:pt-6 max-sm:p-2 md3:p-6">
