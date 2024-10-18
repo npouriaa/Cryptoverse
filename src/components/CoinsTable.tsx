d77c5a182602d33ad5d534a087218a566fe88d0b
npouriaa <npouriaa@gmail.com>
2024-10-18 18:05:09 +0330

    update

--- src/components/CoinsTable.tsx
+++ src/components/CoinsTable.tsx
@@ -3,7 +3,7 @@ import { BiSolidDownArrow } from "react-icons/bi";
 import { CoinType } from "../types/DataTypes";
 import { Skeleton } from "@mui/material";
 import { motion } from "framer-motion";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 type CoinsTableProps = {
   loading: boolean;
