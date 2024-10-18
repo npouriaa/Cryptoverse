d6c72d7315597b861e099859f67189088134b86f
npouriaa <npouriaa@gmail.com>
2024-10-18 16:27:59 +0330

    set footer link text transform to underline

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="w-full h-14 border-t-[1px] border-[#1A1A1A] flex justify-center items-center gap-1">
-      Made by <Link to="https://www.linkedin.com/in/npouriaa" target="_blank" className="text-[#FF9332]">Pouria</Link>
+      Made by <Link to="https://www.linkedin.com/in/npouriaa" target="_blank" className="text-[#FF9332] underline">Pouria</Link>
     </footer>
   );
 };
